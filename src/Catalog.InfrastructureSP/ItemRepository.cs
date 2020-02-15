3127500fd511d92bdf7a9403892e4dbfc71d4286
Jose Ramirez <josemramirez81@outlook.com>
2020-02-15 12:08:08 -0500

    Implementing Data Access using Dapper

---
+++ src/Catalog.InfrastructureSP/ItemRepository.cs
@@ -0,0 +1,51 @@
+﻿using Catalog.Domain.Entities;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace Catalog.InfrastructureSP
+{
+    public class ItemRepository
+    {
+        private readonly SqlConnection _sqlConnection;
+
+        public ItemRepository(string connectionString)
+        {
+            _sqlConnection = new SqlConnection(connectionString);
+        }
+
+        public async Task<IEnumerable<Item>> GetAsync()
+        {
+            var result = await _sqlConnection.QueryAsync<Item>
+                ("GetAllItems", commandType: CommandType.StoredProcedure);
+            return result.AsList();
+        }
+
+        public async Task<Item> GetAsync(Guid id)
+        {
+            return await _sqlConnection.ExecuteScalarAsync<Item>
+                ("GetAllItems", new { Id = id.ToString() },
+                commandType: CommandType.StoredProcedure);
+        }
+
+        public Item Add(Item order)
+        {
+            var result = _sqlConnection.ExecuteScalar<Item>
+                ("InsetItem", order, commandType: CommandType.StoredProcedure);
+            return result;
+        }
+
+        public Item Update(Item item)
+        {
+            var result = _sqlConnection.ExecuteScalar<Item>
+                ("UpdateItem", item, commandType: CommandType.StoredProcedure);
+            return result;
+        }
+
+    }
+    
+
+}
