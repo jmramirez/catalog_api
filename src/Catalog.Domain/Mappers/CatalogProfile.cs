997fbf3962a2e36b7f9b1cfa0c24290d4ac2c916
Jose Ramirez <josemramirez81@outlook.com>
2020-02-17 13:02:33 -0500

    Defining AutoMapper Profile

--- src/Catalog.Domain/Mappers/CatalogProfile.cs
+++ src/Catalog.Domain/Mappers/CatalogProfile.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using AutoMapper;
+using Catalog.Domain.Entities;
+using Catalog.Domain.Requests.Items;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Catalog.Domain.Mappers
 {
-    class CatalogProfile
+    public class CatalogProfile : Profile
     {
+        public CatalogProfile()
+        {
+            CreateMap<ItemResponse, Item>().ReverseMap();
+            CreateMap<GenreResponse, Genre>().ReverseMap();
+            CreateMap<ArtistResponse, Artist>().ReverseMap();
+            CreateMap<Price, PriceResponse>().ReverseMap();
+            CreateMap<AddItemRequest, Item>().ReverseMap();
+            CreateMap<EditItemRequest, Item>().ReverseMap();
+        }
     }
 }
