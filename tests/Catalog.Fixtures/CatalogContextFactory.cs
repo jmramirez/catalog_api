6dcedf000f3b2834913efb9cd9f194d18852b7b8
Jose Ramirez <josemramirez81@outlook.com>
2020-02-17 14:54:44 -0500

    Refactoring Testing Classes

---
+++ tests/Catalog.Fixtures/CatalogContextFactory.cs
@@ -0,0 +1,38 @@
+﻿using Catalog.Domain.Mappers;
+using Catalog.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalog.Fixtures
+{
+    public class CatalogContextFactory
+    {
+        public readonly TestCatalogContext ContextInstace;
+        public readonly IGenreMapper GenreMapper;
+        public readonly IArtistMapper ArtistMapper;
+        public readonly IItemMapper ItemMapper;
+
+        public CatalogContextFactory()
+        {
+            var contextOptions = new DbContextOptionsBuilder<CatalogContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .EnableSensitiveDataLogging()
+                .Options;
+
+            EnsureCreation(contextOptions);
+            ContextInstace = new TestCatalogContext(contextOptions);
+
+            GenreMapper = new GenreMapper();
+            ArtistMapper = new ArtistMapper();
+            ItemMapper = new ItemMapper(ArtistMapper, GenreMapper);
+        }
+
+        public void EnsureCreation(DbContextOptions<CatalogContext> contextOptions)
+        {
+            using var context = new TestCatalogContext(contextOptions);
+            context.Database.EnsureCreated();
+        }
+    }
+}
