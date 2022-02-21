e6333f680722ad8b13310f0bfa7a0bddca38e2b2
Jose Ramirez <josemramirez81@outlook.com>
2022-02-21 15:10:31 -0500

    Create DbContext, abstraction between our application and the database and our unit of work

---
+++ src/Catalog.Infrastructure/CatalogContext.cs
@@ -0,0 +1,25 @@
+using Catalog.Domain.Entities;
+using Catalog.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Infrastructure;
+
+public class CatalogContext : DbContext, IUnitOfWork
+{
+    public const string DEFAULT_SCHEMA = "catalog";
+    
+    public DbSet<Item> Items { get; set; }
+    
+    public CatalogContext(DbContextOptions<CatalogContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+    }
+
+    public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default(CancellationToken))
+    {
+        await SaveChangesAsync(cancellationToken);
+        return true;
+    }
+}
