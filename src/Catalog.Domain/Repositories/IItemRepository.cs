9b74795f4b037ccb0e0a975b9b5e18ffc80add5d
Jose Ramirez <josemramirez81@outlook.com>
2022-02-21 14:59:53 -0500

    Define ItemRepository interface

---
+++ src/Catalog.Domain/Repositories/IItemRepository.cs
@@ -0,0 +1,11 @@
+using Catalog.Domain.Entities;
+
+namespace Catalog.Domain.Repositories;
+
+public interface IItemRepository : IRepository
+{
+    Task<IEnumerable<Item>> GetAsync();
+    Task<Item> GetAsync(Guid id);
+    Item Add(Item item);
+    Item Update(Item item);
+}
