aed2d149e4d3837af627baab14ed1c54e52cd31c
Jose Ramirez <josemramirez81@outlook.com>
2022-02-21 14:57:21 -0500

    Define repository pattern and unit of work

---
+++ src/Catalog.Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+namespace Catalog.Domain.Repositories;
+
+public interface IUnitOfWork : IDisposable
+{
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
+    Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default(CancellationToken));
+}
