0c678f7f5765b2840e64f5972b65726d1aeeef86
Jose Ramirez <josemramirez81@outlook.com>
2022-02-21 21:04:36 -0500

    Implement item request model validation

---
+++ src/Catalog.Domain/Requests/Item/Validators/EditItemRequestValidator.cs
@@ -0,0 +1,17 @@
+using FluentValidation;
+
+namespace Catalog.Domain.Requests.Item.Validators;
+
+public class EditItemRequestValidator : AbstractValidator<EditItemRequest>
+{
+    public EditItemRequestValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty();
+        RuleFor(x => x.GenreId).NotEmpty();
+        RuleFor(x => x.ArtistId).NotEmpty();
+        RuleFor(x => x.Price).NotEmpty();
+        RuleFor(x => x.ReleaseDate).NotEmpty();
+        RuleFor(x => x.Name).NotEmpty();
+        RuleFor(x => x.Price).Must(x => x?.Amount > 0);
+    }
+}
